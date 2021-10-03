35946bab578a29b5b2072fbecb26066b3924f40e
Sindhu Binil <sbtker@gmail.com>
2021-10-03 00:07:09 -0400

    restructured project to support MVC

--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -6,22 +6,31 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 exports.default = router;
-router.get('/', function (req, res, next) {
+
+
+router.get('/', function (req, res, next) 
+{
     res.render('index', { title: 'Home' });
 });
-router.get('/home', function (req, res, next) {
+router.get('/home', function (req, res, next) 
+{
     res.render('index', { title: 'Home' });
 });
-router.get('/about', function (req, res, next) {
+router.get('/about', function (req, res, next) 
+{
     res.render('index', { title: 'About' });
 });
-router.get('/projects', function (req, res, next) {
+router.get('/projects', function (req, res, next) 
+{
     res.render('index', { title: 'Projects' });
 });
-router.get('/services', function (req, res, next) {
+router.get('/services', function (req, res, next) 
+{
     res.render('index', { title: 'Services' });
 });
-router.get('/contact', function (req, res, next) {
+router.get('/contact', function (req, res, next) 
+{
     res.render('index', { title: 'Contact' });
 });
+
 //# sourceMappingURL=index.js.map
